1fc85dfca8be00577ce525352347ce679b809d22
sachun78 <sachun78@gmail.com>
2026-01-10 09:17:11 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+const generatorBtn = document.getElementById('generator-btn');
+const numbersContainer = document.getElementById('numbers-container');
+
+generatorBtn.addEventListener('click', () => {
+  numbersContainer.innerHTML = '';
+  const numbers = new Set();
+  while (numbers.size < 6) {
+    numbers.add(Math.floor(Math.random() * 45) + 1);
+  }
+
+  for (const number of Array.from(numbers).sort((a,b) => a-b)) {
+    const circle = document.createElement('div');
+    circle.classList.add('number-circle');
+    circle.textContent = number;
+    numbersContainer.appendChild(circle);
+  }
+});
